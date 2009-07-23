dc58c91d3bfb0bacbc19b52df40006bc1dbc6e30
orentrutner <orentrutner@4c890986-7748-11de-8e60-65c8b4fa459e>
2009-07-23 05:28:27 +0000

    initial import

---
+++ SortedDictionary/Public/Public-1.moved-aside/SortedDictionaryEntry.h
@@ -0,0 +1,16 @@
+//
+//  SortedDictionaryEntry.h
+//  SortedDictionary
+//
+//  Created by oren on 7/17/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SortedDictionaryEntry : NSObject {
+
+}
+
+@end
